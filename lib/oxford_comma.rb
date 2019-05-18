0fefc7ceddac8cf710abe60ddbc7e94e7a7277c5
Justin <sumbhodi1618@gmail.com>
2019-05-18 03:36:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,7 @@ def oxford_comma(array)
   if array.length <= 2
         array.join(' and ')
     else array.length >= 3 
-      array[-1] = array.join(', ') "and #{array[-1]}"
+      array[-1] = "and #{array[-1]}"
+      array.join(', ')
     end 
 end
